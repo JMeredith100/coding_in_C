5bf2e63d04029a3e78eb7a4da018e24e0997cd80
JMeredith100 <146263218+JMeredith100@users.noreply.github.com>
2023-10-10 16:01:04 +0000

    bootcamp

---
+++ c_bootcamp/third.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main () {
+    // This is a comment
+    int var;
+    printf("Enter a number: ");
+    scanf("%d", &var);
+    printf("var1 = %d\n", var);
+    return 0;
+}
